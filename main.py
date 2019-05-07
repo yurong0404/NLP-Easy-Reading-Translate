8f3c20c9032b03e8c471a8267a32c4f6afb59196
yurong <t3837735@gmail.com>
2019-05-07 17:48:13 +0800

    OK

--- main.py
+++ main.py
@@ -15,64 +15,49 @@ if __name__ == '__main__':
 		words_5000.append([line[0],line[1]])
 	f.close()
 	
-	'''
-	#read input file and encode into token list "paragraph"
-	f = open('input.txt','r')
-	paragraph = []
-	for line in f.readlines():
-		#if line == '\n':
-		#	continue
-		line = line.lower()
-		line = line.strip('\n')
-		temp = []
-		remove_token = [',','.','"',"'s","s'"]
-		for i in line.split(' '):
-			for rm_token in remove_token:
-				print(rm_token,i)
-				while rm_token in i:
-					i = i.replace(rm_token, '')
-			temp.append(i)
-		paragraph = paragraph + temp
-	f.close()
-
-	print('Origin:\n')
-	print(paragraph)
-
-	verb_pos = ['VBD','VBG','VBN','VBP','VBZ']
-	noun_pos = ['NNS']
-	print('Difficult words\n')
-	for index, word in enumerate(paragraph):
-		if word == '':
-			continue
-		print(nltk.pos_tag([word]))
-		if nltk.pos_tag([word])[0][1] in verb_pos:
-			word = WordNetLemmatizer().lemmatize(word.lower(),'v')
-		elif nltk.pos_tag([word])[0][1] in noun_pos:
-			word = WordNetLemmatizer().lemmatize(word.lower(),'n')
-		if word.lower() not in [i[0] for i in words_5000]:
-			print(word)
-	'''
-
 	
 	f = open('input.txt','r')
 	origin = f.read()
 	print('Origin paragraph:')
 	print(origin,'\n')
-	paragraph = nltk.word_tokenize(origin)
-	#print(paragraph)
-	token_pos = nltk.pos_tag(paragraph)
-	#print(token_pos)
+	# paragraph is a list of origin paragraph split by ' '
+	paragraph = origin.split(' ')
+
+	#token_pos => [('Tokyo', 'NNP'), ('(', '('), ('CNN', 'NNP'), (')', ')'), ('Japan', 'NNP'), ("'s", 'POS'), ....]
+	token_pos = nltk.pos_tag(nltk.word_tokenize(origin))
+	
+	#word_token => dictionary with key = paragraph's index, value = word_tokenize(value of paragraph)
+	#word_token => {0: ['Tokyo'], 1:['(', 'CNN', ')', 'Japan', "'s"], 2:['85-year-old'], ....}
+	word_token = {}
+	for index, token in enumerate(paragraph):
+		word_token[index] = nltk.word_tokenize(token)
+
+	#list of (word_token's index, pos, original token, origina token's lower and simple tense || original token)
+	#allword => [(0, 'NNP', 'Tokyo', 'Tokyo'), (1, '(', '(', '('), (1, 'NNP', 'CNN', 'CNN'), .....]
+	allword = []
 	verb_pos = ['VBD','VBG','VBN','VBP','VBZ']
 	noun_pos = ['NNS']
-	for index, word in enumerate(token_pos):
-		if word[1] in verb_pos:
-			paragraph[index] = WordNetLemmatizer().lemmatize(word[0].lower(),'v')
-		elif word[1] in noun_pos:
-			paragraph[index] = WordNetLemmatizer().lemmatize(word[0].lower(),'n')
+	posIndex = 0
+	for key, value in word_token.items():
+		for v in value:
+			current_pos = token_pos[posIndex][1]
+			#simplify the word tense of V. and N.
+			if current_pos in verb_pos:
+				word = (key, current_pos, v, WordNetLemmatizer().lemmatize(v.lower(),'v'))
+				allword.append(word) 
+			elif current_pos in noun_pos:
+				word = (key, current_pos, v, WordNetLemmatizer().lemmatize(v.lower(),'n'))
+				allword.append(word)
+			else:
+				word = (key, current_pos, v, v)
+				allword.append(word)
+			posIndex += 1
+	
 
-	care_pos = ['JJ', 'NN', 'JJR', 'JJS', 'NN', 'NNS', 'RB', 'RBS', 'RP', 'VB', 'VBD', 'VBG', 'VBN', 'VBP',' VBZ']
+	# the POS we check whether the word is difficult.
+	care_pos = ['NN', 'NN', 'NNS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP',' VBZ']
 	print('Difficult words:')
-	for index, token in enumerate(paragraph):
-		if token not in [i[0] for i in words_5000] and token.lower() not in [i[0] for i in words_5000] and token_pos[index][1] in care_pos:
-			print(index, '\t', token, '\t', token_pos[index][1])
+	for word in allword:
+		if word[3] not in [i[0] for i in words_5000] and word[3].lower() not in [i[0] for i in words_5000] and word[1] in care_pos:
+			print(word)
 	
