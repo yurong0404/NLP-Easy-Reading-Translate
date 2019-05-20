a10257f5202dd75e6789971375bc6d133bd4d6be
yurong <t3837735@gmail.com>
2019-05-20 19:28:53 +0800

    OK

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ if __name__ == '__main__':
 		line = line.lower()
 		line = line.strip('\n')
 		line = line.split('\t',1)
-		words_5000.append([line[0],line[1]])
+		words_5000.append([line[0]])
 	f.close()
 	
 	
-	f = open('input.txt','r')
+	f = open('input2.txt','r')
 	origin = f.read()
 	print('Origin paragraph:')
 	print(origin,'\n')
@@ -25,13 +25,13 @@ if __name__ == '__main__':
 
 	#token_pos => [('Tokyo', 'NNP'), ('(', '('), ('CNN', 'NNP'), (')', ')'), ('Japan', 'NNP'), ("'s", 'POS'), ....]
 	token_pos = nltk.pos_tag(nltk.word_tokenize(origin))
-	
+
 	#word_token => dictionary with key = paragraph's index, value = word_tokenize(value of paragraph)
 	#word_token => {0: ['Tokyo'], 1:['(', 'CNN', ')', 'Japan', "'s"], 2:['85-year-old'], ....}
 	word_token = {}
 	for index, token in enumerate(paragraph):
 		word_token[index] = nltk.word_tokenize(token)
-
+	
 	#list of (word_token's index, pos, original token, origina token's lower and simple tense || original token)
 	#allword => [(0, 'NNP', 'Tokyo', 'Tokyo'), (1, '(', '(', '('), (1, 'NNP', 'CNN', 'CNN'), .....]
 	allword = []
